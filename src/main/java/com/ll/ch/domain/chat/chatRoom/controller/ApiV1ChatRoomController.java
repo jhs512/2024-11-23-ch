4c9b89979aaa8d8106ddfad8f33033bb15c2f1fd
jhs512 <jangka512@gmail.com>
2024-11-23 14:31:08 +0900

    work

--- src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
+++ src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
@@ -4,11 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/chat/rooms")
 public class ApiV1ChatRoomController {
     @GetMapping
-    public String getChatRooms() {
-        return "채팅방 목록!";
+    public List<String> getChatRooms() {
+        return List.of(
+                "room1",
+                "room2",
+                "room3"
+        );
     }
 }
