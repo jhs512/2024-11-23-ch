6183b67f403d597d32017ddfbc990e52ac604e3b
jhs512 <jangka512@gmail.com>
2024-11-23 15:20:25 +0900

    work

--- src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
+++ src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
@@ -1,10 +1,7 @@
 package com.ll.ch.domain.chat.chatRoom.controller;
 
 import com.ll.ch.domain.chat.chatRoom.entity.ChatRoom;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,6 +10,9 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/chat/rooms")
+@CrossOrigin(
+        origins = "https://cdpn.io"
+)
 public class ApiV1ChatRoomController {
     private final List<ChatRoom> chatRooms = new ArrayList<>() {{
         // 아래는 샘플데이터 생성코드 입니다.
