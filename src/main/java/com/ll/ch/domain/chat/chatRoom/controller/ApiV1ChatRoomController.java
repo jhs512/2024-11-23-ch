205906e150fafa41f3b6c6dbebf85ae404a866ea
jhs512 <jangka512@gmail.com>
2024-11-23 14:46:20 +0900

    work

--- src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
+++ src/main/java/com/ll/ch/domain/chat/chatRoom/controller/ApiV1ChatRoomController.java
@@ -2,12 +2,14 @@ package com.ll.ch.domain.chat.chatRoom.controller;
 
 import com.ll.ch.domain.chat.chatRoom.entity.ChatRoom;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/chat/rooms")
@@ -48,5 +50,20 @@ public class ApiV1ChatRoomController {
     @GetMapping
     public List<ChatRoom> getChatRooms() {
         return chatRooms;
+    }
+
+    @GetMapping("/{id}")
+    public ChatRoom getChatRoom(
+            @PathVariable long id
+    ) {
+        Optional<ChatRoom> opChatRoom = findById(id);
+
+        return opChatRoom.get();
+    }
+
+    private Optional<ChatRoom> findById(long id) {
+        return chatRooms.stream()
+                .filter(chatRoom -> chatRoom.getId() == id)
+                .findFirst();
     }
 }
